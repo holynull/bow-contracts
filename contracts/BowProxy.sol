7751f1349c157daf099e3cbfa19b70b15332892a
张乐平 <holynull@126.com>
2021-01-30 12:47:49 +0800

    fixed pending bug

--- contracts/BowProxy.sol
+++ contracts/BowProxy.sol
@@ -315,8 +315,14 @@ contract BowProxy is IBowProxy, HRC20, Ownable, ReentrancyGuard {
                 reward.mul(10**18).div(lpSupply)
             );
         }
-        return
+        uint256 tokenBal = IHRC20(tokenAddress).balanceOf(address(walletShare));
+        uint256 pending =
             user.amount.mul(accSushiPerShare).div(10**18).sub(user.rewardDebt);
+        if (tokenBal > pending) {
+            return pending;
+        } else {
+            return tokenBal;
+        }
     }
 
     function massUpdatePools() external noOpenMigration {
